372fc0c85eab6d9c938e67f61fa050597a74b566
JuanAvalo <avalojuanma@gmail.com>
2022-05-11 13:47:16 -0300

    Added JWT logic

---
+++ middlewares/tokenHandler.js
@@ -0,0 +1,32 @@
+const req = require('express/lib/request');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const Unauthorized = require('../errors/unauthorized');
+
+const jwtKey = process.env.JWT_KEY;
+const jwtExpirySeconds = 3600;
+
+//Used to create and send a token to the user after a successfull LogIn
+const createToken = (user) => {
+  const token = jwt.sign({ user }, jwtKey, {
+    algorithm: 'HS256',
+    expiresIn: jwtExpirySeconds,
+  });
+  return token;
+};
+
+//Verify if the auth token sent by the user is valid
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) throw new Unauthorized();
+  jwt.verify(authHeader, jwtKey, function (error, decoded) {
+    if (error) return res.status(500).send({ auth: false, message: error });
+    req.user = decoded;
+    next();
+  });
+};
+
+module.exports = {
+  createToken,
+  verifyToken,
+};
